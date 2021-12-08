1a24e1fb46fbb0403380f4bd50b4eaf17c2e2b8c
tauhq <>
2021-12-08 23:33:31 +0800

    added adsense code

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -22,6 +22,10 @@
  <script
   async
   src="https://www.googletagmanager.com/gtag/js?id={process.env.GTAG}"></script>
+ <script
+  data-ad-client="ca-pub-{process.env.ADSENSE}"
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </svelte:head>
 
 <header>
